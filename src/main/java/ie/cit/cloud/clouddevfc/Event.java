6914bd03eb33092756309fdcdb79b0e1e2ad713d
Frank Crowe <fcrowe07@eircom.ne>
2012-05-12 11:48:17 +0100

    Added class - event

---
+++ src/main/java/ie/cit/cloud/clouddevfc/Event.java
@@ -0,0 +1,37 @@
+package ie.cit.cloud.clouddevfc;
+
+public class Event {
+
+	private int id;
+	private String date;
+	private String venue;
+	private String winner;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	public String getWinner() {
+		return winner;
+	}
+	public void setWinner(String winner) {
+		this.winner = winner;
+	}
+
+}
+
